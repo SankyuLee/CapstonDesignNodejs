8069265c818901fd222d868bc92f0ec0f63fe2d9
SankyuLee <sklee7753@gmail.com>
2019-06-10 17:20:23 +0900

    aa

--- routes/result.js
+++ routes/result.js
@@ -13,7 +13,24 @@ router.get('/', function(req, res, next) {
   var eventId = req.query.eventId;
   //var user_len = results.length;
   connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
-    res.json(results);
+    var res = results;
+    var byUser = [];
+    res.forEach(function(e, i) {
+      if (!this[e.userId]) {
+        this[e.userId] = {
+          userId: e.userId,
+          nickname: e.nickname,
+          items: [{
+            itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
+          }]
+        };
+        byUser.push(this[e.userId])
+      } else {
+        this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
+      }
+    });
+
+    res.json(byUser);
   });
 });
 
