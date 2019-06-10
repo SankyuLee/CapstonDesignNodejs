a83524748ba61d7b89a06a4f67d1bab3c89ec2fe
SankyuLee <sklee7753@gmail.com>
2019-06-11 02:10:48 +0900

    aa

--- routes/excel.js
+++ routes/excel.js
@@ -12,74 +12,78 @@ var router = express.Router();
 
 /* GET excel file. */
 router.get('/', function(req, res, next) {
-  var eventId = req.query.eventId;
-  connection.query("SELECT eventname from events where id=?;", [eventId], function(error, results, fields) {
-    console.log(results);
-    if (results.length == 0)
-      res.end("<h1>Not Valid Event!!!!</h1>");
-    else
-      var eventname = results[0].eventname;
-    connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
+  if (!req.query.eventId)
+    res.end("<h1>No Event!!!!</h1>");
+  else {
+    var eventId = req.query.eventId;
+    connection.query("SELECT eventname from events where id=?;", [eventId], function(error, results, fields) {
+      console.log(results);
       if (results.length == 0)
-        res.end("<h1>Event Not On Result Phase!!!!</h1>");
-      var result = results;
-      var byUser = [];
-      var commons = {};
-      var diff = 0;
+        res.end("<h1>Not Valid Event!!!!</h1>");
+      else
+        var eventname = results[0].eventname;
+      connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
+        if (results.length == 0)
+          res.end("<h1>Event Not On Result Phase!!!!</h1>");
+        var result = results;
+        var byUser = [];
+        var commons = {};
+        var diff = 0;
 
-      result.forEach(function(e, i) {
-        if (!this[e.userId]) {
-          this[e.userId] = {
-            userId: e.userId,
-            nickname: e.nickname,
-            items: [{
-              itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
-            }]
-          };
-          byUser.push(this[e.userId])
-        } else {
-          this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
-        }
-        if (e.o_quantity == 0) {
-          if (!commons[e.id])
-            commons[e.id] = {count: 1, price: e.price};
-          else {
-            commons[e.id]['count']++;
+        result.forEach(function(e, i) {
+          if (!this[e.userId]) {
+            this[e.userId] = {
+              userId: e.userId,
+              nickname: e.nickname,
+              items: [{
+                itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
+              }]
+            };
+            byUser.push(this[e.userId])
+          } else {
+            this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
           }
-        }
-      }, {});
+          if (e.o_quantity == 0) {
+            if (!commons[e.id])
+              commons[e.id] = {count: 1, price: e.price};
+            else {
+              commons[e.id]['count']++;
+            }
+          }
+        }, {});
 
-      for (x in commons) {
-        diff += commons[x]['price'] % commons[x]['count'];
-      }
-      var arr = [];
-      var row_cnt = 1;
-      for (e in byUser) {
-        arr.push(["닉네임", byUser[e]['nickname']]);
-        row_cnt++;
-        arr.push(["항목", "가격", "수량", "합계"]);
-        row_cnt++;
-        var start_row = row_cnt;
-        for (i in byUser[e]['items']) {
-          if (byUser[e]['items'][i].o_quantity == 0)
-            arr.push([byUser[e]['items'][i].itemname, byUser[e]['items'][i].price, "1/"+commons[byUser[e]['items'][i]['itemId']]['count'], Math.floor(commons[byUser[e]['items'][i]['itemId']]['price']/commons[byUser[e]['items'][i]['itemId']]['count'])]);
-          else
-            arr.push([byUser[e]['items'][i].itemname, byUser[e]['items'][i].price, byUser[e]['items'][i].quantity, {t: 'n', f: "=B"+row_cnt+"*C"+row_cnt}]);
+        for (x in commons) {
+          diff += commons[x]['price'] % commons[x]['count'];
+        }
+        var arr = [];
+        var row_cnt = 1;
+        for (e in byUser) {
+          arr.push(["닉네임", byUser[e]['nickname']]);
+          row_cnt++;
+          arr.push(["항목", "가격", "수량", "합계"]);
+          row_cnt++;
+          var start_row = row_cnt;
+          for (i in byUser[e]['items']) {
+            if (byUser[e]['items'][i].o_quantity == 0)
+              arr.push([byUser[e]['items'][i].itemname, byUser[e]['items'][i].price, "1/"+commons[byUser[e]['items'][i]['itemId']]['count'], Math.floor(commons[byUser[e]['items'][i]['itemId']]['price']/commons[byUser[e]['items'][i]['itemId']]['count'])]);
+            else
+              arr.push([byUser[e]['items'][i].itemname, byUser[e]['items'][i].price, byUser[e]['items'][i].quantity, {t: 'n', f: "=B"+row_cnt+"*C"+row_cnt}]);
+            row_cnt++;
+          }
+          arr.push(["총액", "", "", {t: 'n', f: "=SUM(D"+start_row+":D"+(row_cnt-1)+")"}]);
+          row_cnt++;
+          arr.push([""]);
           row_cnt++;
         }
-        arr.push(["총액", "", "", {t: 'n', f: "=SUM(D"+start_row+":D"+(row_cnt-1)+")"}]);
-        row_cnt++;
-        arr.push([""]);
-        row_cnt++;
-      }
-      arr.push(["짤짤이","","",diff]);
-      var ws = XLSX.utils.aoa_to_sheet(arr);
-    	var wb = XLSX.utils.book_new();
-    	XLSX.utils.book_append_sheet(wb, ws, filenamify(eventname));
-    	XLSX.writeFile(wb, 'excel/event'+eventId+'.xlsx');
-      res.download('excel/event'+eventId+'.xlsx', 'event'+eventId+'.xlsx');
+        arr.push(["짤짤이","","",diff]);
+        var ws = XLSX.utils.aoa_to_sheet(arr);
+      	var wb = XLSX.utils.book_new();
+      	XLSX.utils.book_append_sheet(wb, ws, filenamify(eventname));
+      	XLSX.writeFile(wb, 'excel/event'+eventId+'.xlsx');
+        res.download('excel/event'+eventId+'.xlsx', 'event'+eventId+'.xlsx');
+      });
     });
-  });
+  }
 });
 
 module.exports = router;
--- routes/result.js
+++ routes/result.js
@@ -10,50 +10,54 @@ var router = express.Router();
 
 /* GET result of users by eventId. */
 router.get('/', function(req, res, next) {
-  var eventId = req.query.eventId;
-  connection.query("SELECT eventname from events where id=?;", [eventId], function(error, results, fields) {
-    console.log(results);
-    if (results.length == 0)
-      res.end("<h1>Not Valid Event!!!!</h1>");
-    else
-      var eventname = results[0].eventname;
-    connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
+  if (!req.query.eventId)
+    res.end("<h1>No Event!!!!</h1>");
+  else {
+    var eventId = req.query.eventId;
+    connection.query("SELECT eventname from events where id=?;", [eventId], function(error, results, fields) {
+      console.log(results);
       if (results.length == 0)
-        res.end("<h1>Event Not On Result Phase!!!!</h1>");
-      var result = results;
-      var byUser = [];
-      var commons = {};
-      var diff = 0;
+        res.end("<h1>Not Valid Event!!!!</h1>");
+      else
+        var eventname = results[0].eventname;
+      connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
+        if (results.length == 0)
+          res.end("<h1>Event Not On Result Phase!!!!</h1>");
+        var result = results;
+        var byUser = [];
+        var commons = {};
+        var diff = 0;
 
-      result.forEach(function(e, i) {
-        if (!this[e.userId]) {
-          this[e.userId] = {
-            userId: e.userId,
-            nickname: e.nickname,
-            items: [{
-              itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
-            }]
-          };
-          byUser.push(this[e.userId])
-        } else {
-          this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
-        }
-        if (e.o_quantity == 0) {
-          if (!commons[e.id])
-            commons[e.id] = {count: 1, price: e.price};
-          else {
-            commons[e.id]['count']++;
+        result.forEach(function(e, i) {
+          if (!this[e.userId]) {
+            this[e.userId] = {
+              userId: e.userId,
+              nickname: e.nickname,
+              items: [{
+                itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
+              }]
+            };
+            byUser.push(this[e.userId])
+          } else {
+            this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
           }
+          if (e.o_quantity == 0) {
+            if (!commons[e.id])
+              commons[e.id] = {count: 1, price: e.price};
+            else {
+              commons[e.id]['count']++;
+            }
+          }
+        }, {});
+
+        for (x in commons) {
+          diff += commons[x]['price'] % commons[x]['count'];
         }
-      }, {});
 
-      for (x in commons) {
-        diff += commons[x]['price'] % commons[x]['count'];
-      }
-
-      res.render("result", {byUser: byUser, commons: commons, diff: diff, eventname: eventname});
+        res.render("result", {byUser: byUser, commons: commons, diff: diff, eventname: eventname});
+      });
     });
-  });
+  }
 });
 
 module.exports = router;
