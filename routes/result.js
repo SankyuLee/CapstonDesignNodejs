bcad2fbf2f11dfd0d7fd2026dda899f859614cd1
SankyuLee <sklee7753@gmail.com>
2019-06-10 17:47:53 +0900

    aaa

--- routes/result.js
+++ routes/result.js
@@ -15,6 +15,8 @@ router.get('/', function(req, res, next) {
   connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
     var result = results;
     var byUser = [];
+    var commons = {};
+    var diff = 0;
     result.forEach(function(e, i) {
       if (!this[e.userId]) {
         this[e.userId] = {
@@ -28,9 +30,16 @@ router.get('/', function(req, res, next) {
       } else {
         this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
       }
+      if (e.o_quantity == 0) {
+        if (!commons[e.id])
+          commons[e.id] = {count: 1, price: e.price};
+        else {
+          commons[e.id]['count']++;
+        }
+      }
     });
 
-    res.json(byUser);
+    res.json(commons);
   });
 });
 
