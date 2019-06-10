b09af6436be5b2598497f6282da8ab9af34e80e7
SankyuLee <sklee7753@gmail.com>
2019-06-10 20:13:43 +0900

    !!!!!!!!!

--- routes/result.js
+++ routes/result.js
@@ -11,42 +11,44 @@ var router = express.Router();
 /* GET result of users by eventId. */
 router.get('/', function(req, res, next) {
   var eventId = req.query.eventId;
-  connection.query("SELECT e.eventname, i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
-    if (error)
-      console.log(error);
-    var result = results;
+  connection.query("SELECT eventname from events where id=?;", [eventId], function(error, results, fields) {
     var eventname = result[0].eventname;
-    var byUser = [];
-    var commons = {};
-    var diff = 0;
+    connection.query("SELECT e.eventname, i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
+      if (error)
+        console.log(error);
+      var result = results;
+      var byUser = [];
+      var commons = {};
+      var diff = 0;
 
-    result.forEach(function(e, i) {
-      if (!this[e.userId]) {
-        this[e.userId] = {
-          userId: e.userId,
-          nickname: e.nickname,
-          items: [{
-            itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
-          }]
-        };
-        byUser.push(this[e.userId])
-      } else {
-        this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
-      }
-      if (e.o_quantity == 0) {
-        if (!commons[e.id])
-          commons[e.id] = {count: 1, price: e.price};
-        else {
-          commons[e.id]['count']++;
+      result.forEach(function(e, i) {
+        if (!this[e.userId]) {
+          this[e.userId] = {
+            userId: e.userId,
+            nickname: e.nickname,
+            items: [{
+              itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity
+            }]
+          };
+          byUser.push(this[e.userId])
+        } else {
+          this[e.userId]['items'].push({itemId: e.id, itemname: e.itemname, price: e.price, o_quantity: e.o_quantity, quantity: e.quantity});
         }
+        if (e.o_quantity == 0) {
+          if (!commons[e.id])
+            commons[e.id] = {count: 1, price: e.price};
+          else {
+            commons[e.id]['count']++;
+          }
+        }
+      }, {});
+
+      for (x in commons) {
+        diff += commons[x]['price'] % commons[x]['count'];
       }
-    }, {});
 
-    for (x in commons) {
-      diff += commons[x]['price'] % commons[x]['count'];
-    }
-
-    res.render("result", {byUser: byUser, commons: commons, diff: diff, eventname: eventname});
+      res.render("result", {byUser: byUser, commons: commons, diff: diff, eventname: eventname});
+    });
   });
 });
 
