22358d2c75ce492116cd7b0db7ecb4d254fc213c
SankyuLee <sklee7753@gmail.com>
2019-06-10 16:43:45 +0900

    Modify result.js

--- routes/result.js
+++ routes/result.js
@@ -12,7 +12,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var eventId = req.query.eventId;
   //var user_len = results.length;
-  connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM events e, items i, checkLists c, bills b, users u WHERE c.itemId=i.id and i.billId=b.id and b.eventId=? and c.userId=u.id;", [eventId], function(error, results, fields) {
+  connection.query("SELECT i.id, i.itemname, i.price, i.quantity o_quantity, c.quantity, c.userId, u.nickname FROM bills b join items i on b.id=i.billId join checkLists c on i.id=c.itemId join users u on c.userId=u.id WHERE eventId=?;", [eventId], function(error, results, fields) {
     res.json(results);
   });
 });
